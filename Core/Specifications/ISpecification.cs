0ce84bbe19e1b11ec03dbfaad0dcb91dedc5272d
erickmazzocco <erickmazzocco@hotmail.com>
2021-06-06 12:48:36 -0300

    ad

--- Core/Specifications/ISpecification.cs
+++ Core/Specifications/ISpecification.cs
@@ -4,7 +4,7 @@ using System.Linq.Expressions;
 
 namespace Core.Specifications
 {
-    public interface ISpecification<T>
+    public interface ISpecification<T>  
     {
         Expression<Func<T, bool>> Criteria { get; }
         List<Expression<Func<T, object>>> Includes {get; }
